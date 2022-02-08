fb57c48495b370ab013fe299f469813b9ccd06be
Karina Zuniga <mkybun@gmail.com>
2022-02-08 16:08:18 -0600

    online ordering

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
+import 'animate.css'
+
+
+
 export default function Home() {
     return (
         <div>
-            <img className="brulee" alt='brulee' src="https://i.ibb.co/3f3Kwnn/brulee.jpg"/>
+            <img className="brulee" alt='brulee' src="https://i.ibb.co/3f3Kwnn/brulee.jpg" />
+            <div className='container'>
+                <p id='animate' className="animate__animated animate__fadeIn">Online Ordering Now Available!
+                    <div></div>
+                    <button>
+                    <a href="https://www.tasteofpariscarryout.com/">Order Now!</a>
+                    </button></p>
+               
+            </div>
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
+
 export default function Nav() {
   return (
     <div className="nav">
           <div className="leftside">
               <img alt ="logo" src="https://i.ibb.co/X2RGpSB/Taste-Of-Paris-1.png" />
-      </div>
+          </div>
           <div className="rightside">
               <div className="links">
         <p className="about">About</p>
