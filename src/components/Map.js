2d4bdb5b3429d1d138b45182331e67ee4cc2cffd
Karina Zuniga <mkybun@gmail.com>
2022-02-09 16:14:58 -0600

    marker/cleanup

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,34 +1,12 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import Map, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-// import mapboxgl from '!mapbox-gl';
-// eslint-disable-line import/no-webpack-loader-syntax
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoibWt5YnVuIiwiYSI6ImNremZ3NnphcDAxMm0yeHFob3V5YzlkNGcifQ.4a7XtXajYUWmj0DHVoZGAA';
 
 export default function Mapbox() {
 
-
-  // const mapContainer = useRef(null);
-  // const map = useRef(null);
-  // const [lng, setLng] = useState(-88.0024336733652);
-  // const [lat, setLat] = useState(42.26542185364574);
-  // const [zoom, setZoom] = useState(15);
-
-  // useEffect(() => {
-  //     if (map.current) return;
-  //     map.current = new mapboxgl.Map({
-  //     container: mapContainer.current,
-  //     style: 'mapbox://styles/mapbox/dark-v10',
-  //     center: [lng, lat],
-  //     zoom: zoom
-  //     });
-  // });
-
   return (
     <div>
-      {/* <div ref={mapContainer} className="map-container" /> */}
       <Map
         className="mapbox"
         initialViewState={{
