8669170018a88b3d5b80420b35130f3974de0eff
Karina Zuniga <mkybun@gmail.com>
2022-02-11 14:14:37 -0600

    pdf

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,35 @@
+import breakfast from "../breakfast-menu.pdf"
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
+
 export default function Menu() {
+    // const [file, setFile] = useState('public/breakfast-menu.pdf')
+    const [numPages, setNumPages] = useState(null)
+    // const [pageNumber, setPageNumber] = useState(1)
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages)
+        console.log('success')
+}
+
     return (
-        <div className="hello"><h1>Hello!</h1></div>
+        <div className="menu-container">
+            <div className="outermenu">
+                <h1>Breakfast Menu</h1>
+            <iframe className="breakfast" title='menu' src={`${breakfast}#view=fitH`}/>
+
+                    {/* <Document file={file} onLoadSuccess={onDocumentLoadSuccess}
+                >
+           
+           </Document>
+             */}
+            </div>
+        </div>
     )
 }
+
--- src/index.js
+++ src/index.js
@@ -12,23 +12,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// import React from 'react';
-// import { render } from 'react-dom';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import Home from '../src/components/Home'
-// import Menu from '../src/components/Menu'
-
-
-
-// render(
-
-//         <BrowserRouter>
-//             <Routes>
-//                 <Route exact path="/" element={<Home />} />
-//                 <Route exact path="menu" element={<Menu />}/>
-//             </Routes>
-//       </BrowserRouter>,
-//        document.getElementById('root')
-//     )
-
-  
